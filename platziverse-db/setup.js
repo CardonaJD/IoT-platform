7efaa97dff9a39a8de58d2ec4cb37a530f2a8325
Jose David Cardona Rendón <cardonarendonjd@gmail.com>
2019-03-09 14:57:19 -0500

    Introducción a pruebas unitarias con Ava.js

--- platziverse-db/setup.js
+++ platziverse-db/setup.js
@@ -3,40 +3,40 @@
 const debug = require('debug')('platziverse:db:setup')
 const inquirer = require('inquirer')
 const chalk = require('chalk')
-const db = require('./')
+const db = require('./').default
 
 const prompt = inquirer.createPromptModule()
 
-async function setup() {
-    const answer = await prompt([{
-        type: 'confirm',
-        name: 'setup',
-        message: 'This will destroy you database, are you sure?'
-    }])
+async function setup () {
+  const answer = await prompt([{
+    type: 'confirm',
+    name: 'setup',
+    message: 'This will destroy you database, are you sure?'
+  }])
 
-    if (!answer.setup) {
-        return console.log("Nothing happened :)")
-    }
+  if (!answer.setup) {
+    return console.log('Nothing happened :)')
+  }
 
-    const config = {
-        database: process.env.DB_NAME || 'platziverse', // process.env variables de entorno
-        username: process.env.DB_USER || 'platzi',
-        password: process.env.DB_PASS || 'platzi',
-        host: process.env.DB_HOST || 'localhost',
-        dialect: 'postgres',
-        logging: s => debug(s),
-        setup: true
-    }
-    await db(config).catch(hadleFatalError)
+  const config = {
+    database: process.env.DB_NAME || 'platziverse', // process.env variables de entorno
+    username: process.env.DB_USER || 'platzi',
+    password: process.env.DB_PASS || 'platzi',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+    logging: s => debug(s),
+    setup: true
+  }
+  await db(config).catch(hadleFatalError)
 
-    console.log('Successes')
-    process.exit(0)
+  console.log('Successes')
+  process.exit(0)
 
-    function hadleFatalError(err) {
-        console.error(`${chalk.red('[fatal error]')} ${err.message}`)
-        console.error(err.stack)
-        process.exit(1)
-    }
+  function hadleFatalError (err) {
+    console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+    console.error(err.stack)
+    process.exit(1)
+  }
 }
 
 setup()
